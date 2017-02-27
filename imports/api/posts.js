3705f439457decc7ee1ae0f8cd79898cc8866960
Ana Chua <anaprimawaty@gmail.com>
2017-02-27 08:21:52 -0800

    Able to like and unlike. Needs testing and refactoring

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -14,12 +14,12 @@ Meteor.methods({
   'posts.insert'(title, body) {
     check(title, String);
     check(body, String);
- 
-    // Make sure the user is logged in before inserting a task
+
+    // Make sure the user is logged in before inserting a post
     if (! Meteor.user()) {
       throw new Meteor.Error('not-authorized');
     }
- 
+
     Posts.insert({
       title: title,
       body: body,
@@ -27,5 +27,37 @@ Meteor.methods({
       author: Meteor.user()._id,
       username: Meteor.user().username
     });
+  },
+
+  'posts.like'(post) {
+    // Make sure the user is logged in before liking a post
+    if (! Meteor.user()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Posts.update({_id: post}, {$inc: {liked_count: 1} });
+  },
+
+  'posts.unlike'(post) {
+    // Make sure the user is logged in before unliking a post
+    if (! Meteor.user()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Posts.update({_id: post}, {$inc: {liked_count: -1} });
+  },
+
+  'users.like'(post, user) {
+    // Make sure the user is logged in before appending to liked_posts
+    if (! Meteor.user()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Meteor.users.update({_id: user._id}, {$push: {'profile.liked_posts': post}});
+  },
+
+  'users.unlike'(post, user) {
+    // Make sure the user is logged in before removing from liked_posts
+    if (! Meteor.user()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Meteor.users.update({_id: user._id}, {$pull: {'profile.liked_posts': post}});
   },
 });
--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -1,12 +1,48 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Post extends Component {
+    unlike(currentPost, currentUser) {
+      // Decrement like_count of current post
+      Meteor.call('posts.unlike', currentPost);
+      // Remove current post id from liked_posts array of user
+      Meteor.call('users.unlike', currentPost, currentUser);
+    }
+
+    like(currentPost, currentUser) {
+      // Increment like_count of current post
+      Meteor.call('posts.like', currentPost);
+      // Add current post id to liked_posts array of user
+      Meteor.call('users.like', currentPost, currentUser);
+    }
+
+    renderLikeButton() {
+      const currentPost = this.props.post._id;
+      const currentUser = Meteor.user();
+      console.log("currentPost:", currentPost);
+      console.log("currentUser:", currentUser);
+
+      if (currentUser.profile != undefined && currentUser.profile.liked_posts != undefined) {
+        const likedPosts = currentUser.profile.liked_posts
+        console.log("likedPosts:", likedPosts);
+        if (likedPosts.indexOf(currentPost) != -1) {
+          // Current user liked this post. Clicking should unlike
+          return <button onClick={this.unlike.bind(this, currentPost, currentUser)}>Unlike</button>
+        }
+      }
+
+      // Current user did not like this post. Clicking should like
+      return <button onClick={this.like.bind(this, currentPost, currentUser)}>Like</button>
+    }
+
     render() {
         return (
             <div>
+              Id: {this.props.post._id}<br/>
               Title: {this.props.post.title}<br/>
               Body: {this.props.post.body}<br/>
               Username: {this.props.post.username}<br/>
+              No. of Likes: { this.props.post.liked_count }<br/>
+              { this.renderLikeButton() }
             </div>
         );
     }
