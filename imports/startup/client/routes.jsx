e993bb9207e8a6eb4e9f686bd028c6d8244bb57a
Annabel Tay <annabel.tay17@gmail.com>
2017-02-27 21:58:37 -0800

    Add files

---
+++ imports/startup/client/routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+import App from '../../ui/App.jsx';
+import LandingPage from '../../ui/LandingPage.jsx';
+import Main from '../../ui/Main.jsx';
+
+Meteor.startup(() => {
+	render(
+		<Router history={browserHistory}>
+			<Route path="/" component={Main}>
+				<IndexRoute component={LandingPage} />
+				<Route path="/post" component={App} />
+			</Route>
+		</Router>,
+		document.getElementById( 'render-target' )
+	);
+});
+
---
+++ imports/ui/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const LandingPage = () => {
+	return (
+		<div>
+			<h1>LANDING PAGE</h1>
+		</div>
+	);
+};
+
+export default LandingPage;
