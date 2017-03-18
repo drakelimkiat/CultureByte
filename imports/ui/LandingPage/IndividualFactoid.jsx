e776e2557cb80527f93c3f029967fc5b87d14356
Annabel Tay <annabel.tay17@gmail.com>
2017-03-17 18:21:08 -0700

    Fix clashing css bug

--- imports/ui/LandingPage/IndividualFactoid.jsx
+++ imports/ui/LandingPage/IndividualFactoid.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IndividualFactoid = ({title, location, snippet, wrapperClass}) => {
    	return (
       <div className={wrapperClass}>
-        <div className="caption">
+        <div className="factoid">
           <h1>{title}</h1>
           <p>{snippet}</p>
         </div>
