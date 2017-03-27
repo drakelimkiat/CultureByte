f17177c5ad63d4ab5aff86074923b8575a598d8c
Annabel Tay <annabel.tay17@gmail.com>
2017-03-27 01:19:21 -0700

    Re-indent

--- imports/ui/LandingPage/TeamMember.jsx
+++ imports/ui/LandingPage/TeamMember.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const TeamMember = ({name, role, description, imageClassName}) => {
-   	return (
-		<div className="team-member">
-			<div className={imageClassName}></div>
-			<div className="member-info">
-				<h3>{name}</h3>
-				<h4>{role}</h4>
-				<p>{description}</p>
-			</div>
-		</div>
-   	);
+  return (
+    <div className="team-member">
+      <div className={imageClassName}></div>
+      <div className="member-info">
+        <h3>{name}</h3>
+        <h4>{role}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default TeamMember;
