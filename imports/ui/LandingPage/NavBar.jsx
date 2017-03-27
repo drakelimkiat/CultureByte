13797ab25d104f21e66d2baeebc624a682d677da
Annabel Tay <annabel.tay17@gmail.com>
2017-03-26 21:08:19 -0700

    Remove contribution and post navigation items from nav bar when no user is logged in

--- imports/ui/LandingPage/NavBar.jsx
+++ imports/ui/LandingPage/NavBar.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { IndexLink, Link } from 'react-router';
+import { createContainer } from 'meteor/react-meteor-data';
 import AccountsUIWrapper from '../AccountsUIWrapper.jsx';
+import { Meteor } from 'meteor/meteor';
 
-const NavBar = ({currentPath}) => {
-	const currentUser = Meteor.user();
-	console.log(currentUser);
-	return (
-		<div className="header">
-			<ul className="main-navigation">
-				<li className={currentPath == "/" ? 'current' : ''}><IndexLink to="/">Home</IndexLink></li>
-				<li className={currentPath == "/contribution" ? 'current' : ''}><Link to="/contribution">Contribution</Link></li>
-				<li className={currentPath == "/post" ? 'current' : ''}><Link to="/post">Post</Link></li>
-				<li id="login-button" className="highlight with-sep">
-					<AccountsUIWrapper />
-				</li>
-			</ul>
-		</div>
-	);
+class NavBar extends Component {
+	constructor(props) {
+			super(props);
+	}
+
+	render() {
+		const currentPath = this.props.currentPath;
+		return (
+			<div className="header">
+				<ul className="main-navigation">
+					<li className={currentPath == "/" ? 'current' : ''}><IndexLink to="/">Home</IndexLink></li>
+					<li className={this.props.currentUser ? (currentPath == "/contribution" ? 'current' : '') : 'hide'}><Link to="/contribution">Contribution</Link></li>
+					<li className={this.props.currentUser ? (currentPath == "/post" ? 'current' : '') : 'hide'}><Link to="/post">Post</Link></li>
+					<li id="login-button" className="highlight with-sep">
+						<AccountsUIWrapper />
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+NavBar.propTypes = {
+    currentUser: PropTypes.object
 };
 
-export default NavBar;
+export default createContainer(() => {
+    return {
+        currentUser: Meteor.user()
+    };
+}, NavBar);
