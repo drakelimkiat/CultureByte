0a98c1d84374c87c13753347e12273edff3f2aed
Annabel Tay <annabel.tay17@gmail.com>
2017-03-26 23:06:29 -0700

    Change functional components into class components

--- imports/ui/LandingPage/IndividualPrompt.jsx
+++ imports/ui/LandingPage/IndividualPrompt.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 import { Link } from 'react-router';
 
-const IndividualPrompt = ({title, subtitle, imageUrl, wrapperClass}) => {
-   	return (
-      <div className={wrapperClass}>
-        <img src={imageUrl} />
+class IndividualPrompt extends Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+    return (
+      <div className={this.props.wrapperClass}>
+        <img src={this.props.imageUrl} />
         <div className="caption">
-          <h1 className="animated fadeInLeftBig">{title}</h1>
-          <p className="animated fadeInLeftBig">{subtitle}</p>
+          <h1 className="animated fadeInLeftBig">{this.props.title}</h1>
+          <p className="animated fadeInLeftBig">{this.props.subtitle}</p>
           <Link to="/contribution"><span data-scroll className="btn btn-start animated fadeInRightBig">SHARE</span></Link>
         </div>
       </div>
-   	);
+    );
+  }
+}
+
+IndividualPrompt.propTypes = {
+    currentUser: PropTypes.object,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    imageUrl: PropTypes.string,
+    wrapperClass: PropTypes.string
 };
 
-export default IndividualPrompt;
+export default createContainer(() => {
+    return {
+        currentUser: Meteor.user()
+    };
+}, IndividualPrompt);
