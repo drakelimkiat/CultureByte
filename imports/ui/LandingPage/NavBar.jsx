88ee8704a0797c03e521d7d2a68d01451abe794a
Annabel Tay <annabel.tay17@gmail.com>
2017-03-27 14:40:25 -0700

    Clean up CSS for NavBar

--- imports/ui/LandingPage/NavBar.jsx
+++ imports/ui/LandingPage/NavBar.jsx
@@ -11,17 +11,18 @@ class NavBar extends Component {
 
 	render() {
 		const currentPath = this.props.currentPath;
+		const hideIfNoUserIsLoggedIn = this.props.currentUser ? '' : 'hide'
 		return (
 			<div className="header">
 				<ul className="main-navigation">
-					<li className={currentPath == "/" ? 'current' : ''}>
-						<IndexLink to="/">Home</IndexLink>
+					<li>
+						<IndexLink activeClassName={currentPath == "/" ? 'current' : ''} to="/">Home</IndexLink>
 					</li>
-					<li className={this.props.currentUser ? (currentPath == "/contribution" ? 'current' : '') : 'hide'}>
-						<Link to="/contribution">Contribution</Link>
+					<li className={hideIfNoUserIsLoggedIn}>
+						<Link activeClassName={currentPath == "/contribution" ? 'current' : ''} to="/contribution">Contribution</Link>
 					</li>
-					<li className={this.props.currentUser ? (currentPath == "/post" ? 'current' : '') : 'hide'}>
-						<Link to="/post">Post</Link>
+					<li className={hideIfNoUserIsLoggedIn}>
+						<Link activeClassName={currentPath == "/post" ? 'current' : ''} to="/post">Post</Link>
 					</li>
 					<li id="login-button" className="highlight with-sep">
 						<AccountsUIWrapper />
