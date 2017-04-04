91202422b5a79fe4e2e5f8207b40ce859def83a6
Annabel Tay <annabel.tay17@gmail.com>
2017-04-04 02:31:10 -0700

    Initial implementation for responsive title section

--- imports/ui/LandingPage/TitleSection.jsx
+++ imports/ui/LandingPage/TitleSection.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const TitleSection = () => {
-  return (
-    <div className="intro">
-      <div className="intro-content">
-        <div className="cb-row">
-          <div className="col-twelve">
-            <h5>Hello welcome to Culture Bytes.</h5>
-            <h1>We introduce you to interesting culture facts</h1>
-          </div>
+  var MediaQuery = require('react-responsive');
+  var body = (
+    <div>
+      <div className="cb-row">
+        <div className="col-twelve">
+          <h5>Hello welcome to Culture Bytes.</h5>
+          <h1>We introduce you to interesting culture facts</h1>
         </div>
       </div>
-    </div>
-  );
-};
+    </div>);
 
-export default TitleSection;
+    return (
+      <div id="intro">
+        <div className="intro-content">
+          <MediaQuery minDeviceWidth={1025}>{body}</MediaQuery>
+          <MediaQuery maxDeviceWidth={1024} minDeviceWidth={769} className="large-screen">{body}</MediaQuery>
+          <MediaQuery maxDeviceWidth={768} minDeviceWidth={601} className="medium-screen">{body}</MediaQuery>
+          <MediaQuery maxDeviceWidth={600} className="small-screen">{body}</MediaQuery>
+        </div>
+      </div>
+    );
+  };
+
+  export default TitleSection;
