94995cb08f4abed9e3d596f4465c753d6ffa796a
Lim Kiat <limkiat@u.nus.edu>
2017-04-04 02:50:23 -0700

    First attempt at showing correct page depending if signed in

--- imports/ui/LandingPage/NavBar.jsx
+++ imports/ui/LandingPage/NavBar.jsx
@@ -3,25 +3,32 @@ import { IndexLink, Link } from 'react-router';
 import { createContainer } from 'meteor/react-meteor-data';
 import AccountsUIWrapper from '../AccountsUIWrapper.jsx';
 import { Meteor } from 'meteor/meteor';
+import { browserHistory } from 'react-router';
 
 class NavBar extends Component {
 	constructor(props) {
 			super(props);
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (Meteor.user()) {
+			console.log("user signed in");
+			browserHistory.push('/post');
+		}
+	}
+
 	render() {
 		const hideIfNoUserIsLoggedIn = this.props.currentUser ? '' : 'hide'
+		const homePageRoute = this.props.currentUser ? '/post' : '/'
+
 		return (
 			<div className="header">
 				<ul className="main-navigation">
 					<li>
-						<IndexLink activeClassName='current' to="/">Home</IndexLink>
+						<IndexLink activeClassName='current' to={homePageRoute}><span className="logo">CultureByte</span></IndexLink>
 					</li>
 					<li className={hideIfNoUserIsLoggedIn}>
-						<Link activeClassName='current' to="/contribution">Contribution</Link>
-					</li>
-					<li className={hideIfNoUserIsLoggedIn}>
-						<Link activeClassName='current' to="/post">Post</Link>
+						<Link activeClassName='current' to="/contribution">Your Posts</Link>
 					</li>
 					<li id="login-button" className="highlight with-sep">
 						<AccountsUIWrapper />
