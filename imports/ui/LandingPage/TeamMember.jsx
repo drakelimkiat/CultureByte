a274cc37299e723b721624b9596d46c1c7725580
Annabel Tay <annabel.tay17@gmail.com>
2017-04-04 18:32:48 -0700

    Complete making team section responsive

--- imports/ui/LandingPage/TeamMember.jsx
+++ imports/ui/LandingPage/TeamMember.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
 const TeamMember = ({name, role, description, imageClassName}) => {
-  return (
-    <div className="team-member">
-      <div className={imageClassName}></div>
-      <div className="member-info">
-        <h3>{name}</h3>
-        <h4>{role}</h4>
-        <p>{description}</p>
+  const MediaQuery = require('react-responsive');
+  const body = (
+    <div>
+      <div className="team-member">
+        <div className={imageClassName}></div>
+        <div className="member-info">
+          <h3>{name}</h3>
+          <h4>{role}</h4>
+          <p>{description}</p>
+        </div>
       </div>
+    </div>
+  );
+  return (
+    <div>
+      <MediaQuery minDeviceWidth={601} className="desktop-screen">{body}</MediaQuery>
+      <MediaQuery maxDeviceWidth={600} className="mobile-screen">{body}</MediaQuery>
     </div>
   );
 };
--- imports/ui/LandingPage/TeamSection.jsx
+++ imports/ui/LandingPage/TeamSection.jsx
@@ -2,48 +2,58 @@ import React from 'react';
 import TeamMember from './TeamMember.jsx';
 
 const TeamSection = () => {
-   	return (
-   		<div className="team">
-   			<div className="cb-row">
-   			    <div className="team-page-header">
-          			<h2>The Team</h2>
-          			<p>Introducing our relatively young group of founders</p>
-        		</div>
-   			</div>
-   			<div className="cb-row">
-   				<TeamMember
-   					name="Ana"
-   					role="CPO"
-   					description="She decides which features make the cut!"
-   					imageClassName="img ana"
-   					key="Ana" />
-   				<TeamMember
-   					name="Si Kai"
-   					role="CTO"
-   					description="He decides all things tech!"
-   					imageClassName="img sikai"
-   					key="SiKai" />
-   				<TeamMember
-   					name="Celeste"
-   					role="CDO"
-   					description="She decides all things data related!"
-   					imageClassName="img celeste"
-   					key="Celeste" />
-   				<TeamMember
-   					name="Lim Kiat"
-   					role="CFO"
-   					description="He does our financial projections!"
-   					imageClassName="img limkiat"
-   					key="LimKiat" />
-   				<TeamMember
-   					name="Annabel"
-   					role="CEO"
-   					description="She sells CultureByte to investors!"
-   					imageClassName="img annabel"
-   					key="Annabel" />
-   			</div>
-   		</div>
-   	);
+  const MediaQuery = require('react-responsive');
+  const body = (
+    <div>
+      <h2>The Team</h2>
+      <p>Introducing our relatively young group of founders</p>
+    </div>
+  );
+
+  return (
+    <div className="team">
+      <div className="cb-row">
+        <div className="team-page-header">
+          <div>
+            <MediaQuery minDeviceWidth={601} className="desktop-screen">{body}</MediaQuery>
+            <MediaQuery maxDeviceWidth={600} className="mobile-screen">{body}</MediaQuery>
+          </div>
+        </div>
+      </div>
+      <div className="cb-row">
+        <TeamMember
+          name="Ana"
+          role="CPO"
+          description="She decides which features make the cut!"
+          imageClassName="img ana"
+          key="Ana" />
+        <TeamMember
+          name="Si Kai"
+          role="CTO"
+          description="He decides all things tech!"
+          imageClassName="img sikai"
+          key="SiKai" />
+        <TeamMember
+          name="Celeste"
+          role="CDO"
+          description="She decides all things data related!"
+          imageClassName="img celeste"
+          key="Celeste" />
+        <TeamMember
+          name="Lim Kiat"
+          role="CFO"
+          description="He does our financial projections!"
+          imageClassName="img limkiat"
+          key="LimKiat" />
+        <TeamMember
+          name="Annabel"
+          role="CEO"
+          description="She sells CultureByte to investors!"
+          imageClassName="img annabel"
+          key="Annabel" />
+      </div>
+    </div>
+  );
 };
 
 export default TeamSection;
