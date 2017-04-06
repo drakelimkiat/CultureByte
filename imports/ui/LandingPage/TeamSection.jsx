a4d6f91e2ee8b481a1c126756384265386c79832
Annabel Tay <annabel.tay17@gmail.com>
2017-04-05 20:04:08 -0700

    Improve responsiveness of team section

--- imports/ui/LandingPage/TeamSection.jsx
+++ imports/ui/LandingPage/TeamSection.jsx
@@ -9,7 +9,40 @@ const TeamSection = () => {
       <p>Introducing our relatively young group of founders</p>
     </div>
   );
-
+  const teamMembers = (
+    <div className="cb-row">
+      <TeamMember
+        name="Ana"
+        role="CPO"
+        description="She decides the features!"
+        imageClassName="img ana"
+        key="Ana" />
+      <TeamMember
+        name="Si Kai"
+        role="CTO"
+        description="He decides all things tech!"
+        imageClassName="img sikai"
+        key="SiKai" />
+      <TeamMember
+        name="Celeste"
+        role="CDO"
+        description="She decides all things data related!"
+        imageClassName="img celeste"
+        key="Celeste" />
+      <TeamMember
+        name="Lim Kiat"
+        role="CFO"
+        description="He does our financial projections!"
+        imageClassName="img limkiat"
+        key="LimKiat" />
+      <TeamMember
+        name="Annabel"
+        role="CEO"
+        description="She sells CultureByte to investors!"
+        imageClassName="img annabel"
+        key="Annabel" />
+    </div>
+  );
   return (
     <div className="team">
       <div className="cb-row">
@@ -20,38 +53,9 @@ const TeamSection = () => {
           </div>
         </div>
       </div>
-      <div className="cb-row">
-        <TeamMember
-          name="Ana"
-          role="CPO"
-          description="She decides which features make the cut!"
-          imageClassName="img ana"
-          key="Ana" />
-        <TeamMember
-          name="Si Kai"
-          role="CTO"
-          description="He decides all things tech!"
-          imageClassName="img sikai"
-          key="SiKai" />
-        <TeamMember
-          name="Celeste"
-          role="CDO"
-          description="She decides all things data related!"
-          imageClassName="img celeste"
-          key="Celeste" />
-        <TeamMember
-          name="Lim Kiat"
-          role="CFO"
-          description="He does our financial projections!"
-          imageClassName="img limkiat"
-          key="LimKiat" />
-        <TeamMember
-          name="Annabel"
-          role="CEO"
-          description="She sells CultureByte to investors!"
-          imageClassName="img annabel"
-          key="Annabel" />
-      </div>
+      <MediaQuery minWidth={901} id="large-screen">{teamMembers}</MediaQuery>
+      <MediaQuery minWidth={601} maxWidth={900} id="medium-screen">{teamMembers}</MediaQuery>
+      <MediaQuery maxWidth={600} id="small-screen">{teamMembers}</MediaQuery>
     </div>
   );
 };
