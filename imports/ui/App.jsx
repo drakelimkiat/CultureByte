e197f396b3fee5945e9641a2f3baba27c1f576c3
Ana Chua <anaprimawaty@gmail.com>
2017-04-10 10:14:27 -0700

    First attempt

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -9,17 +9,68 @@ import Post from './Post.jsx';
 import Form from './Form.jsx';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 import { browserHistory } from 'react-router';
-import {Grid, Row, Col} from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Infinitez } from 'react-infinite';
 
 // App component - represents the whole app
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: 0,
-      popIndex: 0,
-      sortType: 'time'
+      sortType: 'time',
+      elements: undefined,
+      isInfiniteLoading: false,
     };
+  }
+
+  buildElements(start, end, p) {
+    console.log("buildElements " + p);
+    if (p == undefined) {
+      p = this.props.posts;
+    }
+    if (p[0]) {
+      end = Math.min(p.length, end);
+      var elements = [];
+      for (var i=start; i<end; i++) {
+        if (this.state.sortType == 'time') {
+          elements.push(<Post
+            key={p[i]._id}
+            post={p[i]}
+            type="post" />);
+        } else if (this.state.sortType == 'pop') {
+          elements.push(<Post
+            key={p[i]._id}
+            post={p[i]}
+            type="post" />);
+        }
+      }
+      return elements;
+    }
+  }
+
+  handleInfiniteLoad() {
+    var that = this;
+    setTimeout(function() {
+      if (that.state.elements) {
+        that.setState({
+          isInfiniteLoading: true,
+        });
+        var elemLength = that.state.elements.length,
+          newElements = that.buildElements(elemLength, elemLength + 1);
+        that.setState({
+          isInfiniteLoading: false,
+          elements: that.state.elements.concat(newElements)
+        });
+      }
+    }, 2500);
+  }
+
+  elementInfiniteLoad() {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
   }
 
   componentWillUpdate() {
@@ -29,217 +80,100 @@ class App extends Component {
   }
 
   componentWillReceiveProps(newProps) {
-    if (newProps.posts.length == 0) {
-      return;
-    }
-
-    if (newProps.posts.length != this.props.posts.length) {
-      if (this.state.sortType == 'time') {
-        this.setState({
-          index: 0
-        });
-      } else if (this.state.sortType == 'pop') {
-        this.setState({
-          popIndex: newProps.popPosts.length - 1
-        });
-      }
-      return;
+    console.log("componentWillReceiveProps newProps: " + newProps.posts);
+    if (newProps && newProps.posts[0]) {
+      console.log("componentWillReceiveProps " + "success");
+      this.setState({
+        elements: this.buildElements(0, 2, newProps.posts)
+      });
+      console.log("componentWillReceiveProps " + "success2");
+    } else {
+      console.log("componentWillReceiveProps " + "failed");
     }
   }
 
-  renderPost() {
+  renderCreatePostForm() {
+    return <Form/>
+  }
+
+  renderToggleButton() {
     if (this.state.sortType == 'time') {
-      if (this.props.posts[0]) {
-        return <Post
-          key={this.props.posts[this.state.index]._id}
-          post={this.props.posts[this.state.index]}
-          type="post" />
-      }
+      return (
+        <div className="category">
+          <button className="category-button enable" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>New <i className="fa fa-clock-o" aria-hidden="true"></i></button>
+          <button className="category-button" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>Hot <i className="fa fa-fire" aria-hidden="true"></i></button>
+        </div>
+      );
     } else if (this.state.sortType == 'pop') {
-      if (this.props.popPosts[0]) {
-        return <Post
-          key={this.props.popPosts[this.state.popIndex]._id}
-          post={this.props.popPosts[this.state.popIndex]}
-          type="post" />
-      }
+      return (
+        <div className="category">
+          <button className="category-button" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>New <i className="fa fa-clock-o" aria-hidden="true"></i></button>
+          <button className="category-button enable" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>Hot <i className="fa fa-fire" aria-hidden="true"></i></button>
+        </div>
+      );
     }
   }
 
-  renderNextButton() {
-    if (this.props.posts.length == 0) {
-      return;
-    }
-
-    let index = null;
-    let postArray = null;
+  onToggleBetweenTimeAndPop() {
     if (this.state.sortType == 'time') {
-      index = this.state.index;
-      postArray = this.props.posts;
+      this.setState({
+        sortType: 'pop'
+      })
     } else if (this.state.sortType == 'pop') {
-      index = this.state.popIndex;
-      postArray = this.props.popPosts;
+      this.setState({
+        sortType: 'time'
+      })
     }
-    // Check if we are at the end of the list
-    if (postArray.length != index + 1) {
-      return <button className="next pagination-button" onClick={this.nextPost.bind(this)}><i className="fa fa-arrow-right" aria-hidden="true"></i></button>;
-      } else {
-        return <button className="next pagination-button" disabled><i className="fa fa-arrow-right" aria-hidden="true"></i></button>;
-        }
-      }
+  }
 
-      renderBackButton() {
-        if (this.props.posts.length == 0) {
-          return;
-        }
+  render() {
+    document.title = 'Post';
+    let toggleButton = null;
+    let createPostForm = null;
+    if (this.props.currentUser) {
+      toggleButton  = this.renderToggleButton();
+      createPostForm = this.renderCreatePostForm();
+    }
+    console.log("rendering " + this.state.elements);
+    return (
+      <Grid>
+        <div className="content">
+          <Row>
+            <Col lg={6} lgOffset={2} md={8} sm={8} xs={12}>
+              {toggleButton}
+              <Infinite elementHeight={300}
+                         containerHeight={1000}
+                         infiniteLoadBeginEdgeOffset={200}
+                         onInfiniteLoad={this.handleInfiniteLoad.bind(this)}
+                         loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                         isInfiniteLoading={this.state.isInfiniteLoading}>
+                {this.state.elements}
+              </Infinite>
+            </Col>
+            <Col lg={3} lgPull={1} md={4} sm={4} xs={12}>
+              {createPostForm}
+            </Col>
+          </Row>
+        </div>
+      </Grid>
+    );
+  }
+}
 
-        let index = null;
-        if (this.state.sortType == 'time') {
-          index = this.state.index;
-        } else if (this.state.sortType == 'pop') {
-          index = this.state.popIndex;
-        }
-        // Check if we are at the start of the list
-        if (index != 0) {
-          return <button className="back pagination-button" onClick={this.previousPost.bind(this)}><i className="fa fa-arrow-left" aria-hidden="true"></i></button>;
-          } else {
-            return <button className="back pagination-button" disabled><i className="fa fa-arrow-left" aria-hidden="true"></i></button>;
-            }
-          }
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+  popPosts: PropTypes.array.isRequired,
+  currentUser: PropTypes.object
+};
 
-          renderCreatePostForm() {
-            return <Form/>
-          }
+export default createContainer(() => {
+  Meteor.subscribe('posts');
 
-          nextPost() {
-            // Track next post in Google Analytics
-            analytics.track('NextPost', {
-              Username: Meteor.user().username,
-              UserId: Meteor.user()._id
-            });
-
-            if (this.state.sortType == 'time') {
-              this.setState({
-                index: this.state.index + 1
-              });
-              analytics.track('NewNextPost', {
-                Username: Meteor.user().username,
-                UserId: Meteor.user()._id
-              });
-            } else if (this.state.sortType == 'pop') {
-              this.setState({
-                popIndex: this.state.popIndex + 1
-              });
-              analytics.track('HotNextPost', {
-                Username: Meteor.user().username,
-                UserId: Meteor.user()._id
-              });
-            }
-          }
-
-          previousPost() {
-            // Track previous post in Google Analytics
-            analytics.track('PreviousPost', {
-              Username: Meteor.user().username,
-              UserId: Meteor.user()._id
-            });
-
-            if (this.state.sortType == 'time') {
-              this.setState({
-                index: this.state.index - 1
-              });
-              analytics.track('NewPreviousPost', {
-                Username: Meteor.user().username,
-                UserId: Meteor.user()._id
-              });
-            } else if (this.state.sortType == 'pop') {
-              this.setState({
-                popIndex: this.state.popIndex - 1
-              });
-              analytics.track('HotPreviousPost', {
-                Username: Meteor.user().username,
-                UserId: Meteor.user()._id
-              });
-            }
-          }
-
-          renderToggleButton() {
-            if (this.state.sortType == 'time') {
-              return (
-                <div className="category">
-                  <button className="category-button enable" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>New <i className="fa fa-clock-o" aria-hidden="true"></i></button>
-                  <button className="category-button" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>Hot <i className="fa fa-fire" aria-hidden="true"></i></button>
-                </div>
-              );
-            } else if (this.state.sortType == 'pop') {
-              return (
-                <div className="category">
-                  <button className="category-button" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>New <i className="fa fa-clock-o" aria-hidden="true"></i></button>
-                  <button className="category-button enable" onClick={this.onToggleBetweenTimeAndPop.bind(this)}>Hot <i className="fa fa-fire" aria-hidden="true"></i></button>
-                </div>
-              );
-            }
-          }
-
-          onToggleBetweenTimeAndPop() {
-            if (this.state.sortType == 'time') {
-              this.setState({
-                sortType: 'pop'
-              })
-            } else if (this.state.sortType == 'pop') {
-              this.setState({
-                sortType: 'time'
-              })
-            }
-          }
-
-          render() {
-            document.title = 'Post';
-            let postView = null;
-            let toggleButton = null;
-            let nextButton = null;
-            let backButton = null;
-            let createPostForm = null;
-            if (this.props.currentUser) {
-              postView = this.renderPost();
-              toggleButton  = this.renderToggleButton();
-              nextButton = this.renderNextButton();
-              backButton = this.renderBackButton();
-              createPostForm = this.renderCreatePostForm();
-            }
-            return (
-              <Grid>
-                <div className="content">
-                  <Row>
-                    <Col lg={6} lgOffset={2} md={8} sm={8} xs={12}>
-                      {toggleButton}
-                      {postView}
-                      {backButton}
-                      {nextButton}
-                    </Col>
-                    <Col lg={3} lgPull={1} md={4} sm={4} xs={12}>
-                      {createPostForm}
-                    </Col>
-                  </Row>
-                </div>
-              </Grid>
-            );
-          }
-        }
-
-        App.propTypes = {
-          posts: PropTypes.array.isRequired,
-          popPosts: PropTypes.array.isRequired,
-          currentUser: PropTypes.object
-        };
-
-        export default createContainer(() => {
-          Meteor.subscribe('posts');
-
-          return {
-            posts: Posts.find({}, {
-              sort: { createdAt: -1 }
-            }).fetch(),
-            popPosts: Posts.find({}).fetch(),
-            currentUser: Meteor.user()
-          };
-        }, App);
+  return {
+    posts: Posts.find({}, {
+      sort: { createdAt: -1 }
+    }).fetch(),
+    popPosts: Posts.find({}).fetch(),
+    currentUser: Meteor.user()
+  };
+}, App);
